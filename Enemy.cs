1268380c79c5d518ee9e9153eaf51cc2d1121891
windspots <114032928+windspots@users.noreply.github.com>
2022-10-12 10:15:23 -0300

    Modifique Enemy
    Ahora el enimogo puedo mirir

---
+++ Enemy.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] float _life;
+
+    Material _myMaterial;
+
+    Color _initialColor;
+
+    Coroutine _getHitFeedback_Cor;
+
+    public void GetHit(float dmg)
+    {
+        _life -= dmg;
+
+        if (_getHitFeedback_Cor != null)
+        {
+            StopCoroutine(_getHitFeedback_Cor);
+        }
+
+        if (_life <= 0)
+        {
+            Death();
+        }
+        else
+        {
+            _getHitFeedback_Cor = StartCoroutine(GetHitFeedback());
+        }
+    }
+
+    void Death()
+    {
+        StartCoroutine(ReduceToDeath());
+    }
+
+    IEnumerator GetHitFeedback()
+    {
+        float ticks = 0;
+
+        float quantity = 2 / 0.1f;
+
+        while (ticks < quantity)
+        {
+            _myMaterial.color = Color.red;
+
+            yield return new WaitForSeconds(0.1f);
+
+            _myMaterial.color = _initialColor;
+
+            yield return new WaitForSeconds(0.1f);
+
+            ticks++;
+        }
+
+        _getHitFeedback_Cor = null;
+        
+    }
+
+    IEnumerator ReduceToDeath()
+    {
+        Vector3 initialScale = transform.localScale;
+
+        float ticks = 0;
+
+        _myMaterial.color = Color.red;
+
+        while (ticks < 1)
+        {
+            ticks += Time.deltaTime;
+
+            transform.localScale = Vector3.Lerp(initialScale, Vector3.zero, ticks);
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
