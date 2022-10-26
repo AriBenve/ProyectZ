21f7a0c72cdb9536dcbc5310418584d50c4e1317
AriBenve <arielbenveniste@gmail.com>
2022-10-26 00:39:48 -0300

    Fov Dash

--- Assets/Scripts/Jugador/PlayerCam.cs
+++ Assets/Scripts/Jugador/PlayerCam.cs
@@ -31,4 +31,9 @@ public class PlayerCam : MonoBehaviour
         transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, _yRotation, 0);
     }
+
+    public void DoFov(float endValue)
+    {
+        GetComponent<Camera>().fieldOfView = endValue;
+    }
 }
