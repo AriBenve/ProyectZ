629d891dcf0f912dcb5d7d5ef6c71e63177de142
windspots <lewickielian@gmail.com>
2023-02-26 02:31:15 -0300

    Chaeger Enemy

---
+++ Assets/Scripts/Enemy/Golem/Golem.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem : Movent_Enemy
+{
+    [SerializeField] float Speed;
+    [SerializeField] int ChargerDistance;
+    [SerializeField] float WalkDistance;
+    [SerializeField] int timer;
+    float Distance;
+    
+    
+    [SerializeField] GameObject[] hitbox;
+    int hit_Select;
+
+    protected override void Update()
+    {
+        Distance = Vector3.Distance(player.position, this.transform.position);
+
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, PlayerMask);
+
+        enemy.SetDestination(player.position);
+
+        _anim.SetFloat("Speed", enemy.velocity.magnitude);
+
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, PlayerMask);
+
+        _anim.SetBool("Attacking", playerInSightRange && playerInAttackRange);
+
+        if (ChargerDistance <= Distance && Distance > WalkDistance)
+        {
+            enemy.speed = 10;
+            _anim.SetBool("Charging", true);
+            
+        }
+        else if(Distance >= WalkDistance)
+        {
+            enemy.speed = 5;
+            _anim.SetBool("Charging", false);
+        }
+
+
+        if (playerInSightRange && playerInAttackRange)
+        {
+            _anim.SetBool("Attacking", true);
+        }
+        else
+        {
+            _anim.SetBool("Attacking", false);
+        }
+    }
+
+    public void ColliderWeaponTrue()
+    {
+        hitbox[hit_Select].GetComponent<CapsuleCollider>().enabled = true;
+    }
+    public void ColliderWeaponFalse()
+    {
+        hitbox[hit_Select].GetComponent<CapsuleCollider>().enabled = false;
+    }
+
+    private void Charge()
+    {
+        
+
+
+
+    }
+
+  
+
+
+
+
+
+
+
+}
